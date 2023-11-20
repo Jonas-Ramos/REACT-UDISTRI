ec536e60a01406fe767c9c6a73bef5268838b6b2
Jony Monroy <jonymonroy@gmail.com>
2023-11-20 09:17:40 -0500

    React: Uso de variables en JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 
 function App() {
+  const nombre = "Jony";
+  const edad = 12
+  
   return (
   <>
-    <h1>Hola</h1>
-    <p>Lorem ipsum dolor sit amet.</p>
+    <h1>Hola {nombre} </h1>
+    <p>{edad > 18 ? "Adulto": "Menor de edad"}</p>
   </>)
 }
 
