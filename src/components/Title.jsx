6a7e5f31269607fe7bc3ad9d11d287811d58708a
Jony Monroy <jonymonroy@gmail.com>
2023-11-20 14:23:20 -0500

    React: Tipos de datos para propiedades

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,16 +1,24 @@
+import PropTypes from "prop-types";
 
-const Title = ( props) => {
+const Title = ( { text, small }) => {
   return (
     <>
-        <h1>{props.text}</h1>
-        <h3>{props.small}</h3>
+        <h1>{text}</h1>
+        <h3>{small}</h3>
     </>
   )
 }
 
+// Propiedades por defecto
 Title.defaultProps = {
     text: "Admin de Tareas",
-    small: "App"
+    small: "App",
+}
+
+// Define tipos de parámetros
+Title.propTypes = {
+    text : PropTypes.string,
+    small : PropTypes.string
 }
 
 export default Title
