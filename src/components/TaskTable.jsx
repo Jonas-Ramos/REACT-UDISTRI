db049e1bd0bb55229c6dfc77e01c83713df4e8a6
Jony Monroy <jonymonroy@gmail.com>
2023-11-20 15:12:49 -0500

    React: Readem actulizado y componente TaskTable con nueva columna

--- src/components/TaskTable.jsx
+++ src/components/TaskTable.jsx
@@ -1,9 +1,9 @@
 const tasks = [
-    {id: 1, text: "Running"},
-    {id: 2, text: "Reading"},
-    {id: 3, text: "Playing Videogames"},
-    {id: 4, text: "Sleeping"},
-    {id: 5, text: "Movies"},
+    {id: 1, text: "Running", completed: false},
+    {id: 2, text: "Reading", completed: true},
+    {id: 3, text: "Playing Videogames", completed: true},
+    {id: 4, text: "Sleeping", completed: false},
+    {id: 5, text: "Movies", completed: false},
 ];
 
 
@@ -13,14 +13,17 @@ const TaskTable = () => {
         <tr>
             <th>ID</th>
             <th>Hobbie</th>
+            <th>Completed</th>
         </tr>
     </thead>
     <tbody>
         {
+            // Recorre la lista
             tasks.map(task => ( 
             <tr key={task.id}>
                 <td>{ task.id }</td>
                 <td>{ task.text }</td>
+                <td>{ `${task.completed}`}</td>
             </tr> ))
         }
     </tbody>
