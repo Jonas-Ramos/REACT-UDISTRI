2c41e66a035d46f263d65b640cadca602809cf18
Jony Monroy <jonymonroy@gmail.com>
2023-11-21 10:28:58 -0500

    React: CSS GLOBAL

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './App.css'
 import Header from './components/Header';
 import TaskList from './components/TaskList';
 
+
 function App() {
   return (
-    <>
+    <div className='app'>
       <Header />
       <TaskList />
-    </>
+    </div>
   );
 }
 
